3e3f1dc5e79f52b1e83f2d7216c4b643218b9bfb
ruben <scheedler.r@tkppensioen.nl>
2022-10-07 10:48:22 +0200

    Add spring initialzr code

---
+++ src/test/java/nl/codeclan/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.codeclan.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
